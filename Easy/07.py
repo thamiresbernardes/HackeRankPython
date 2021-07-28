3f0cc9b4b962cc06cb6af5261d923a7908f842a6
Thamires Bernardes <thamires.bernardes@gmail.com>
2021-07-28 20:56:29 -0300

    evo

---
+++ Easy/07.py
@@ -0,0 +1,5 @@
+# USANDO LAMBDA (FUNÇÕES ANONIMAS)
+
+list(map(lambda i: print(i, end=''), [i for i in range(1, int(input()) + 1)]))
+
+# A função map() serve para aplicarmos uma função a cada um dos elementos passado em lista como argumento a ela.
